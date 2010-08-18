c69180551e1b68fc11c89b42cde00f06422d030c
mutantturkey <crazycal00@gmail.com>
2010-08-17 21:28:15 -0400

    good to go :)

--- basename.c
+++ basename.c
@@ -2,40 +2,31 @@
 #include <stdio.h>
 
 int main( int argc, char * argv[]) {
-
-char *basename;
-char *str;
-char *final = malloc(256);
-int i = 0;
-int b = 0;
-int a = 0;
-char *suffix;
+ char *basename, *str, *suffix;
+ char *final = malloc(256);
+ int a = 0;
+ int b = 0;
+ int i = 0;
 
     if(argc == 1 | argc > 3) {  return 0;  }
      str = strtok(argv[1], "/");
      
-     while(str != NULL) {
-        basename = str;
-        str = strtok(NULL, "/");       
-     }
-
+        while(str != NULL) {
+            basename = str;
+            str = strtok(NULL, "/");       
+        }
     if(argv[2]) {
-        a = strlen(basename); 
-        b = strlen(argv[2]);
-        suffix = argv[2];
-
+      suffix = argv[2];
+      a = strlen(basename); 
+      b = strlen(suffix);
+      
         for(i=0; i < b+1; i++) {
-
-
             if(basename[a-i] == suffix[b-i]) {
-            } else { break; }
-
-  }
-  strncpy(final, basename, sizeof(basename) - i +1 ); 
-  } else { final = basename; }
-
+         } else break; 
+        }
+      strncpy(final, basename, sizeof(basename) - i +1 ); 
+    } 
+     else final = basename;  
     puts(final);
-    
-
     return 1;
 };
